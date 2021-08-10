cdaebc4baf7159b6ad88b7b66c6e8a04c882f1e5
Cassandra <cparisi1290@gmail.com>
2021-08-10 14:34:01 -0500

    create variable to figure out position of the top of a box that will trigger a box to slide over, iterate over the node list of boxes from querySelectorAll, create condition that if the top of a box is less than the triggered bottom, then the class show will be added to element and remove when it is not, i.e. scrolling down and up

--- script.js
+++ script.js
@@ -3,6 +3,20 @@ const boxes = document.querySelectorAll('.box');
 window.addEventListener('scroll', checkBoxes);
 
 checkBoxes();
+
 function checkBoxes() {
   const triggerBottom = (window.innerHeight / 5) * 4;
+
+  boxes.forEach((box) => {
+    // returns a DOM rect (the size and position of a rectangle)
+    // providing info about the size of an element and its position
+    // relative to the viewpost
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add('show');
+    } else {
+      box.classList.remove('show');
+    }
+  });
 }
